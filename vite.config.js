b085bd77bc45aecbeff796de49f5bb81309d6346
aqiyahzh <iampotato5555@gmail.com>
2025-12-16 22:54:00 +0800

    fix vite config remove missing frame.css

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/frame.css',
                 'resources/js/app.js',
                 'resources/js/script.js',
             ],
